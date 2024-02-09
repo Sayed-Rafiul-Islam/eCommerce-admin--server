8be6815b3970c4d1aa45c4e68057405910de0ce6
Sayed Rafiul Islam <sayed80@student.sust.edu>
2024-02-10 03:44:08 +0600

    done

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,7 +7,6 @@ const getOrders = async (req,res) => {
     try {
         const {storeId} = req.params
         const orders = await Order.find({storeId}).sort({ createdAt : -1}).populate("orderedItems.orderedItem")
-        console.log(orders[0].orderedItems)
         res.status(200).json(orders)
     } catch (error) {
         res.status(500).send(error)
